9785fa2ded96561f62da3a4edf65caf632db5ab5
David Meade Jr <50832271+davidmeadejr@users.noreply.github.com>
2022-01-05 15:14:26 +0000

    Fix/ Removed Imports Not used
    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,6 @@ import {
   animate,
   transition,
 } from '@angular/animations';
-import { OuterCircleComponent } from './components/outer-circle/outer-circle.component';
-import { InnerCircleComponent } from './components/inner-circle/inner-circle.component';
-import { MinButtonComponent } from './components/min-button/min-button.component';
-import { MaxButtonComponent } from './components/max-button/max-button.component';
-import { CircularSliderComponent } from './components/circular-slider/circular-slider.component';
-import { CircularSliderModalComponent } from './components/circular-slider-modal/circular-slider-modal.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
